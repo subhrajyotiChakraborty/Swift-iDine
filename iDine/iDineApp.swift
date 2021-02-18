9a1df6398bb1069d22ce9654bcfd12cea4d78498
Subhrajyoti <subha.ionic3@gmail.com>
2021-02-18 12:42:49 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Subhrajyoti Chakraborty on 18/02/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
